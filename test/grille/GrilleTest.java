d322e8d0b21546d7755cf072c671e30241d3076a
akala kouma <>
2018-06-10 16:34:45 +0000

    Ecriture des classes de Test

--- test/grille/GrilleTest.java
+++ test/grille/GrilleTest.java
@@ -1,14 +1,144 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grille;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
 /**
+ * Classe de test d'une grille sudoku.
  *
- * @author KAM-WAL
+ * @author AKALA Kouma
  */
 public class GrilleTest {
-    
+
+    /**
+     * Variable statique permettant de manipuler plus facilement les caracteres
+     * vides.
+     */
+    static final char EMPTY = Grille.EMPTY;
+
+    /**
+     * Grille vide.
+     */
+    static final char[][] GRILLE_VIDE_9 = {
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}};
+
+    /**
+     * Grille incomplete.
+     */
+    static final char[][] GRILLE_INCOMPLETE_9 = {
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, '6', EMPTY, '7', EMPTY},
+        {EMPTY, '7', EMPTY, '9', EMPTY, '4', EMPTY, '3', EMPTY},
+        {'4', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, '1'},
+        {'3', EMPTY, '6', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+        {EMPTY, EMPTY, '2', '8', EMPTY, '1', '9', EMPTY, EMPTY},
+        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, '1', EMPTY, '5'},
+        {'1', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, '7'},
+        {EMPTY, '3', EMPTY, '5', EMPTY, '8', EMPTY, '9', EMPTY},
+        {EMPTY, '5', EMPTY, '4', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}};
+
+    /**
+     * Grille complete.
+     */
+    static final char[][] GRILLE_COMPLETE_9 = {
+        {'8', '2', '7', '1', '5', '4', '3', '9', '6'},
+        {'9', '6', '5', '3', '2', '7', '1', '4', '8'},
+        {'3', '4', '1', '6', '8', '9', '7', '5', '2'},
+        {'5', '9', '3', '4', '6', '8', '2', '7', '1'},
+        {'4', '7', '2', '5', '1', '3', '6', '8', '9'},
+        {'6', '1', '8', '9', '7', '2', '4', '3', '5'},
+        {'7', '8', '6', '2', '3', '5', '9', '1', '4'},
+        {'1', '5', '4', '7', '9', '6', '8', '2', '3'},
+        {'2', '3', '9', '8', '4', '1', '5', '6', '7'}};
+
+
+    /**
+     * Méthode principale.
+     * @param args arguments de la méthode main
+     */
+    public static void main(final String[] args) {
+        GrilleTest grilleTest = new GrilleTest();
+        grilleTest.testGetDimension();
+        grilleTest.testSetValue();
+        grilleTest.testGetValue();
+        grilleTest.testSetValueLeveesDExceptions();
+        grilleTest.testGetValueLeveesDExceptions();
+        grilleTest.testComplete();
+        grilleTest.testPossibleLeveesDExceptions();
+        grilleTest.testPossible();
+    }
+
+    /**
+     * Permet de tester la methode getDimension().
+     */
+    @Test
+    public final void testGetDimension() {
+       //TODO
+    }
+
+    /**
+     * Permet de tester la methode setValue().
+     */
+    @Test
+    public final void testSetValue() {
+        //TODO
+    }
+
+    /**
+     * Permet de tester la methode getValue().
+     */
+    @Test
+    public final void testGetValue() {
+        //TODO
+    }
+
+    /**
+     * Permet de tester la levee d'exceptions pour la methode setValue().
+     */
+    @Test
+    public final void testSetValueLeveesDExceptions() {
+        //TODO
+    }
+
+    /**
+     * Permet de tester la levee d'exceptions pour la methode getValue().
+     */
+    @Test
+    public final void testGetValueLeveesDExceptions() {
+        //TODO
+    }
+
+    /**
+     * Permet de tester la methode complete().
+     */
+    @Test
+    public final void testComplete() {
+        //TODO
+    }
+
+    /**
+     * Permet de tester la levee d'exceptions pour la methode possible().
+     */
+    @Test
+    public final void testPossibleLeveesDExceptions() {
+        //TODO
+
+    }
+
+    /**
+     * Permet de tester la methode possible().
+     */
+    @Test
+    public final void testPossible() {
+        //TODO
+    }
 }
--- test/solveur/SolveurTest.java
+++ test/solveur/SolveurTest.java
@@ -1,14 +1,125 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package solveur;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import grille.GrilleImpl;
 
 /**
  *
- * @author KAM-WAL
+ * Classe de test du solveur de grille.
+ *
+ * @author AKALA Kouma
+ *
  */
-public class SolveurTest {
-    
+public final class SolveurTest {
+
+    /**
+     * Grille correcte.
+     */
+    private static final char[][] GRILLE_CORRECTE
+            = {{'5', '3', '4', '6', '7', '8', '9', '1', '2'},
+            {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
+            {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
+            {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
+            {'4', '2', '6', '8', '5', '3', '7', '9', '1'},
+            {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
+            {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
+            {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
+            {'3', '4', '5', '2', '8', '6', '1', '7', '9'}};
+
+    /**
+     * Grille incorrecte. 5 est présent 2 fois sur la première colonne.
+     */
+    private static final char[][] GRILLE_INCORRECTE
+            = {{'5', '3', '4', '6', '7', '8', '9', '1', '2'},
+            {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
+            {'5', '9', '8', '3', '4', '2', '5', '2', '7'},
+            {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
+            {'4', '2', '6', '8', '5', '3', '7', '9', '1'},
+            {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
+            {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
+            {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
+            {'3', '4', '5', '2', '8', '6', '1', '7', '9'}};
+
+    /**
+     * Grille incorrecte.
+     * 2 est présent 2 fois sur la troisième ligne.
+     * Des caractères non autorisés sont présents dans la grille (x, g).
+     */
+    private static final char[][] GRILLE_INCORRECTE_2
+            = {{'5', '3', '4', '6', '7', '8', '9', '1', '2', 'x'},
+            {'6', '7', '2', '1', '9', '5', '3', '4', '8', '1'},
+            {'5', '9', '8', '3', '4', '2', '5', '2', '7', 't'},
+            {'8', '5', '9', '7', '6', '1', '4', '2', '3', '5'},
+            {'4', '2', '6', '8', '5', '3', '7', '9', '1', '5'},
+            {'7', '1', '3', '9', '2', '4', '8', '5', '6', 'g'},
+            {'9', '6', '1', '5', '3', '7', '2', '8', '4', '1'},
+            {'2', '8', '7', '4', '1', '9', '6', '3', '5', '2'},
+            {'3', '4', '5', '2', '8', '6', '1', '7', '9', '9'}};
+
+    /**
+     * Grille incorrecte. T et X présents dans la grille.
+     */
+    private static final char[][] GRILLE_INCORRECTE_3
+            = {{'X', 'T', '4', '6', '7', '8', '9', '1', '2'},
+            {'X', '7', '@', '1', '9', '5', '3', '4', '8'},
+            {'1', '9', '8', '3', '@', '2', '5', '6', '7'},
+            {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
+            {'4', '2', '@', '8', '5', '3', '7', '9', '1'},
+            {'7', '1', '3', '9', '2', '4', '@', '5', '6'},
+            {'9', '6', '1', '5', '@', '7', '2', '8', '4'},
+            {'@', '8', '7', '4', '1', '9', '6', '@', '5'},
+            {'3', '4', '5', '2', 't', '6', '1', '7', '9'}};
+
+    /**
+     * Test du constructeur de GrillImpl.
+     */
+    @Test
+    public void testConstructeurGrilleImpl() {
+        //TODO
+    }
+
+    /**
+     * Test de la méthode verifierGrille().
+     *
+     */
+    @Test
+    public void testVerifierGrille() {
+        //TODO
+    }
+
+    /**
+     * Test de la méthode resoudre().
+     */
+    @Test
+    public void testResoudre() {
+        //TODO
+    }
+
+    /**
+     * Test de la méthode resolu() pour une grille complète.
+     */
+    @Test
+    public void testResolu() {
+        //TODO
+    }
+
+    /**
+     * Test de la méthode resolu()avec exception pour une grille incorrecte.
+     */
+    @Test
+    public void testResoluAvecException() {
+        //TODO
+    }
+
+    /**
+     * Test de la méthode afficherSolution().
+     */
+    @Test
+    public void testafficherSolution() {
+        //TODO
+    }
+
 }
